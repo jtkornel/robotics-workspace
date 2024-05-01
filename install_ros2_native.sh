ac305cf8ea8f14951a0cedb91f35c802bbb4a92c
Jan Tore Korneliussen <jtkornel@yahoo.no>
2024-05-01 17:30:05 +0200

    Do not build nav2 from source, install apt binaries

--- install_ros2_native.sh
+++ install_ros2_native.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 UBUNTU_VERSION=$(lsb_release -rs)
-if [ $UBUNTU_VERSION == "22.04" ]; then
+if [ "$UBUNTU_VERSION" == "22.04" ]; then
     ROS2_DEFAULT_VERSION="humble"
-elif [ $UBUNTU_VERSION == "20.04" ]; then
+elif [ "$UBUNTU_VERSION" == "20.04" ]; then
     ROS2_DEFAULT_VERSION="foxy"
-elif [ $UBUNTU_VERSION == "18.04" ]; then
+elif [ "$UBUNTU_VERSION" == "18.04" ]; then
     ROS2_DEFAULT_VERSION="dashing"
 else
     echo "Unsupported Ubuntu version: $UBUNTU_VERSION"
@@ -23,9 +23,10 @@ sudo add-apt-repository universe -y
 sudo apt update && sudo apt install curl gnupg lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo "$UBUNTU_CODENAME") main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 sudo apt update
 sudo apt upgrade
 
-sudo apt install ros-$ROS2_VERSION-desktop ros-dev-tools python3-rosdep python3-colcon-common-extensions -y
+sudo apt install "ros-$ROS2_VERSION-desktop" ros-dev-tools python3-rosdep python3-colcon-common-extensions -y
+sudo apt install "ros-$ROS2_VERSION-navigation2" "ros-$ROS2_VERSION-nav2-bringup"
