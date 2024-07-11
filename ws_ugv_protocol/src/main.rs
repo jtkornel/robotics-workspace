854b8b5a3d478b00a932da86af7cbb01708c48f9
Jan Tore Korneliussen <jtkornel@yahoo.no>
2024-07-11 23:44:32 +0200

    Get rid of Comm object
    Just a single mutable reference to the serial port can be held, so the comm struct with two structs is not viable

--- ws_ugv_protocol/src/main.rs
+++ ws_ugv_protocol/src/main.rs
@@ -7,15 +7,12 @@ use ws_ugv_protocol::*;
 use std::io::Cursor;
 
 fn main() {
-    let rx_message = r#"{"T":1001,"L":0,"R":0,"gx":0,"gy":0,"gz":0,"ax":0,"ay":0,"az":0,"r":0,"p":0,"y":0,"q0":0, "q1":0, "q2":0, "q3":0,"odl":0,"odr":0,"v":11.0}"#;
-    let tx_message: Vec<u8> = Vec::new();
 
-    let rx_buff = BufReader::new(Cursor::new(rx_message));
-    let tx_buff = Cursor::new(tx_message);
+    let port = serialport::new("/dev/ttyUSB0", 9600).open().unwrap();
 
-    let mut comm = UGVComm{ command_writer: tx_buff, feedback_reader: rx_buff};
+    //let mut comm = UGVComm{ command_writer: &port, feedback_reader: BufReader::new(port) };
 
-    let deserialized : FeedbackMessage = comm.read_feedback().unwrap();
+    //let deserialized : FeedbackMessage = comm.read_feedback().unwrap();
 
-    println!("{:?}", deserialized);
+    //println!("{:?}", deserialized);
 }
