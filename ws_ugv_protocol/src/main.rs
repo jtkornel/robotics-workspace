2f08cdc1de612ae7214589b84eb49a18a080342c
Jan Tore Korneliussen <jtkornel@yahoo.no>
2024-07-28 20:41:23 +0200

    Updated to use tokio async

--- ws_ugv_protocol/src/main.rs
+++ ws_ugv_protocol/src/main.rs
@@ -1,26 +1,41 @@
-use std::io::BufReader;
 
 use ws_ugv_protocol::messages::*;
 use ws_ugv_protocol::*;
 
-use serial2::SerialPort;
+use tokio_serial::{SerialPort, SerialPortBuilderExt, SerialStream};
+use tokio::io::BufReader;
 
 fn main() {
-    let readport = SerialPort::open("/dev/serial0", 115200).unwrap();
-    let mut reader = BufReader::new(readport);
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let future = app();
+    rt.block_on(future);
+}
 
-    let mut writeport = SerialPort::open("/dev/serial0", 115200).unwrap();
-    writeport.set_rts(false).unwrap();
-    writeport.set_dtr(false).unwrap();
+async fn write_all_commands(mut writeport: SerialStream) {
+    let _ = write_command(& mut writeport, CommandMessage::GetBaseFeedback);
+    let _ = write_command(& mut writeport, CommandMessage::GetIMUData);
+}
+
+async fn read_all_feedback(mut readport: BufReader<SerialStream>) -> (FeedbackMessage, FeedbackMessage) {
+    let base_feedback = read_feedback(& mut readport).await.unwrap();
+    let imu_feedback = read_feedback(& mut readport).await.unwrap();
+
+    (base_feedback, imu_feedback)
+}
+
+async fn app() {
+    let readport = tokio_serial::new("/dev/serial0", 115200).open_native_async().expect("Failed to open port");
+    let breader = BufReader::new(readport);
+
+    let mut writeport = tokio_serial::new("/dev/serial0", 115200).open_native_async().expect("Failed to open port");
+    writeport.write_request_to_send(false).unwrap();
+    writeport.write_data_terminal_ready(false).unwrap();
 
     let flow_off = CommandMessage::SetBaseFeedbackFlow(BaseFeedbackFlowArgs {cmd: 0});
     let _ = write_command(& mut writeport, flow_off);
 
-    let _ = write_command(& mut writeport, CommandMessage::GetBaseFeedback);
-    let msg = read_feedback(& mut reader).unwrap();
-    println!("Received message: {:?}", msg);
-
-    let _ = write_command(& mut writeport, CommandMessage::GetIMUData);
-    let msg = read_feedback(& mut reader).unwrap();
-    println!("Received message: {:?}", msg);
+    let wr = write_all_commands(writeport);
+    let rd = read_all_feedback(breader);
+    wr.await;
+    rd.await;
 }
