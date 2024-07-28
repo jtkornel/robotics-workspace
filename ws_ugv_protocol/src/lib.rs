d663047a29812c34f6a4d90dc9f2c1ba222045f4
Jan Tore Korneliussen <jtkornel@yahoo.no>
2024-07-28 20:41:23 +0200

    Updated to use tokio async

--- ws_ugv_protocol/src/lib.rs
+++ ws_ugv_protocol/src/lib.rs
@@ -1,15 +1,15 @@
-use std::io::{Write, BufRead};
-use std::io;
+use tokio::io::{AsyncWriteExt, AsyncWrite, AsyncBufRead, AsyncBufReadExt};
+use tokio::io;
 
 pub mod messages;
 
 use crate::messages::{CommandMessage, FeedbackMessage};
 
 // Read a feedback message from the UGV
-pub fn read_feedback<R: BufRead>(reader: & mut R) -> Result<FeedbackMessage, io::Error>
+pub async fn read_feedback<R: AsyncBufRead + Unpin>(reader: &mut R) -> io::Result<FeedbackMessage>
 {
     let mut buf = String::new();
-    let num_bytes = reader.read_line(&mut buf)?;
+    let num_bytes = reader.read_line(&mut buf).await?;
 
     if num_bytes == 0
     {
@@ -23,12 +23,12 @@ pub fn read_feedback<R: BufRead>(reader: & mut R) -> Result<FeedbackMessage, io:
 
 // Write a command message to the UGV
 // Returns the number of bytes written
-pub fn write_command<W: Write>(writer: & mut W, msg: CommandMessage)-> Result<usize, io::Error>
+pub async fn write_command<W: AsyncWrite + Unpin>(writer: &mut W, msg: CommandMessage)-> io::Result<usize>
 {
     let json_msg = serde_json::to_string(&msg)?;
-    let num_bytes = writer.write(json_msg.as_bytes())?;
-    writer.write(b"\n")?;
-    Ok(num_bytes)
+    let bytes_body = writer.write(json_msg.as_bytes()).await?;
+    let bytes_terminator = writer.write(b"\n").await?;
+    Ok(bytes_body + bytes_terminator)
 }
 
 
@@ -36,7 +36,8 @@ pub fn write_command<W: Write>(writer: & mut W, msg: CommandMessage)-> Result<us
 mod tests {
 
     use super::*;
-    use std::io::{Cursor, BufReader};
+    use tokio::io::BufReader;
+    use std::io::Cursor;
     use crate::messages::{FeedbackMessage, BaseInfoData, CommandMessage, SpeedArgs};
 
     // Use cursors to simulate the serial port / network connection
@@ -49,30 +50,30 @@ mod tests {
         Cursor::new(tx_message)
     }
 
-    #[test]
-    fn test_read_feedback() {
-        let rx_object = FeedbackMessage::BaseInfo(BaseInfoData{ l: 0.2, r: 0.6, gx: 0.3, gy: 0.4, gz: 0.0, ax: 0.0, ay: 0.0, az: 0.0, r_angle: 0.0, p_angle: 0.0, y_angle: 0.0, q0: 1.0, q1: 0.0, q2: 0.0, q3: 0.0, odl: 0.0, odr: 0.0, v: 11.0, a_b: None, a_s: None, a_e: None, a_t: None, tor_b: None, tor_s: None, tor_e: None, tor_h: None, pan: None, tilt: None});
+    #[tokio::test]
+    async fn test_read_feedback() {
+        let rx_object = FeedbackMessage::BaseInfo(BaseInfoData{ l: 0.2, r: 0.6, gx: Some(0.3), gy: Some(0.4), gz: Some(0.0), ax: Some(0.0), ay: Some(0.0), az: Some(0.0), r_angle: 0.0, p_angle: 0.0, y_angle: Some(0.0), q0: Some(1.0), q1: Some(0.0), q2: Some(0.0), q3: Some(0.0), odl: Some(0.0), odr: Some(0.0), v: 11.0, a_b: None, a_s: None, a_e: None, a_t: None, tor_b: None, tor_s: None, tor_e: None, tor_h: None, pan: None, tilt: None});
         let rx_message = r#"{"T":1001,"L":0.2,"R":0.6,"gx":0.3,"gy":0.4,"gz":0,"ax":0,"ay":0,"az":0,"r":0,"p":0,"y":0,"q0":1.0, "q1":0, "q2":0, "q3":0,"odl":0,"odr":0,"v":11.0}"#;
 
         let mut comm = create_fake_reader(rx_message);
 
-        let deserialized  = read_feedback(&mut comm);
+        let deserialized  = read_feedback(&mut comm).await;
 
         assert!(deserialized.is_ok());
         assert_eq!(deserialized.unwrap(), rx_object);
     }
 
-    #[test]
-    fn test_read_feedback_empty() {
+    #[tokio::test]
+    async fn test_read_feedback_empty() {
         let empty_message: Vec<u8> = Vec::new();
         let mut reader = Cursor::new(empty_message);
-        let deserialized  = read_feedback(&mut reader);
+        let deserialized  = read_feedback(&mut reader).await;
 
         assert!(deserialized.is_err());
     }
 
-    #[test]
-    fn test_write_command() {
+    #[tokio::test]
+    async fn test_write_command() {
         let tx_object = CommandMessage::Speed(SpeedArgs {l: 0.5, r: 0.5});
 
         let mut writer = create_fake_writer();
@@ -80,7 +81,7 @@ mod tests {
         // Nothing written initially
         assert_eq!(writer.get_ref().len(), 0);
 
-        let res = write_command(&mut writer, tx_object);
+        let res = write_command(&mut writer, tx_object).await;
 
         // Check that something was written, with the expected length
         assert!(res.is_ok());
